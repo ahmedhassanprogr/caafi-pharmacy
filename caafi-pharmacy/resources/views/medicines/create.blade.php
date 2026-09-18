8e03a07f736bce93f997ddc838015d32ca87631c
axmed badri <badri736@hotmail.com>
2026-09-18 11:54:11 +0300

    first commit laravel installation and desing model v1

---
+++ caafi-pharmacy/resources/views/medicines/create.blade.php
@@ -0,0 +1,41 @@
+
+
+@extends('layouts.app')
+
+
+
+
+@section('content')
+    <div class="container">
+        <h1>Create Medicine</h1>
+
+        <form action="{{ route('medicines.store') }} " method="POST">
+            @csrf 
+
+            <div class="mb-3">
+                <label for="name" class="form-label">Medicine Name</label>
+                <input type="text" class="form-control" id="name" name="name" required>
+                </div>
+
+                <div class="mb-3">
+                    <label for="description" class="form-label">Description</label>
+                    <textarea class="form-control" id="description" name="description" rows="3" placeholder="Enter medicine description"></textarea>
+                </div>
+
+                <div class="mb-3">
+                <label for="quantity" class="form-label">Quantity</label>
+                <input type="number" min="0" class="form-control" id="quantity" name="quantity" placeholder="Enter quantity" required>
+                </div>
+                <div class="mb-3">
+                <label for="expiry_date" class="form-label">Expiry Date</label>
+                <input type="date" class="form-control" id="expiry_date" name="expiry_date" placeholder="Enter expiry date" required>
+                </div>
+                <div >
+                
+                <button type="submit" class="btn btn-primary mt-3">Create Medicine</button>
+                </div>
+            </div>
+        </form>
+    
+
+@endsection
